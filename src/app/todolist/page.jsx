fa488ddda4b70a9c911988bd6d313725e7942889
MyeongjuHong <hmjpearl@gmail.com>
2025-09-18 15:20:01 +0900

    feat: Implement search feature

--- src/app/todolist/page.jsx
+++ src/app/todolist/page.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 
 const TodolistPage = () => {
   const [todos, setTodos] = useState([]);
+  const [keyword, setKeyword] = useState("");
 
   const handleCheck = async ({ id, isCompleted }) => {
     // const handleCheck = async (a) => {
@@ -54,6 +55,16 @@ const TodolistPage = () => {
     }
   };
 
+  // const handleSearchChange = async (e) => {
+  //   setTodos((prev) =>
+  //     prev.filter((todo) => todo.title.includes(e.target.value))
+  //   );
+  // }; // 오답노트: 원본 todos 변경으로 검색어를 지워도 원래의 전체 목록으로 돌아갈 수 없음 => 처음부터 원본 todo에서 keyword로 filtering한 목록을 map해야함
+
+  const handleSearchChange = (e) => {
+    setKeyword(e.target.value);
+  }; // 오답노트: 값이 바뀔 때마다 원본 배열을 직접 바꾸는게 아닌, keyword를 바꾸고, 그거로 filter한 배열을 출력시켜야 함
+
   useEffect(() => {
     const getLists = async () => {
       const res = await fetch("http://localhost:4000/todos");
@@ -64,6 +75,11 @@ const TodolistPage = () => {
 
     getLists();
   }, []);
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.title.toLowerCase().includes(keyword.toLowerCase()) // 오답노트: todo.title로 해야지!
+  ); // 오답노트: 순전히 데이터를 필터링한 값으로 사용
+
   return (
     <div>
       <h1>[ TODO LIST ]</h1>
@@ -71,11 +87,12 @@ const TodolistPage = () => {
         <input
           placeholder="검색어를 입력하세요"
           className="p-1 border rounded-md"
+          onChange={handleSearchChange}
         />
         <button className="text-blue-500 border p-1 rounded-md">SEARCH</button>
       </form>
       <div className="flex flex-col">
-        {todos.map((t) => {
+        {filteredTodos.map((t) => {
           return (
             <div key={t.id} className="flex items-center gap-4">
               <Link
