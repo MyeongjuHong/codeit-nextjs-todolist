6c5138c874f40059e48a9c1421f2b7040981094e
MyeongjuHong <hmjpearl@gmail.com>
2025-09-18 14:06:48 +0900

    fix: Fix url path

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -1,18 +1,16 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='flex justify-between'>
+    <div className="flex justify-between">
       Home
-      <div className='flex gap-4'>
-        <Link href='todolist'>TODO LISTS</Link>
-        <Link href='new-todo'>CREATE TODO</Link>
-        {/* <Link href='todolist/:id'>TODO DETAIL</Link>
-        // FIXME: todo 텍스트에 표시*/}
+      <div className="flex gap-4">
+        <Link href="/todolist">TODO LISTS</Link>
+        <Link href="/new-todo">CREATE TODO</Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
