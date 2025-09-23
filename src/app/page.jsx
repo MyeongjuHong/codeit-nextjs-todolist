d31f389d70a61c710b3ad9326cdbf74c5a6af2ee
MyeongjuHong <hmjpearl@gmail.com>
2025-09-23 13:35:33 +0900

    feat: Change state management with useQuery

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,29 +3,23 @@
 import { useEffect, useState } from "react";
 import TodoList from "./_components/TodoList";
 import { fetchTodos } from "@/api/todos";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const [todos, setTodos] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
   const loadTodos = async () => {
     const data = await fetchTodos();
     setTodos(data);
   };
 
-  useEffect(() => {
-    try {
-      setIsError(false);
-      setIsLoading(true);
-      loadTodos();
-    } catch (err) {
-      setIsError(true);
-      console.error(err); // REVIEW: 또 console.error로 찍는게 맞나?
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
+  const {
+    data: todos, // NOTE: queryFn(loadTodos) return값을 data 속성에 넣음
+    isPending: isLoading,
+    isError,
+  } = useQuery({
+    // NOTE: key가 없으면 새로 등록 & 함수 매핑해 실행, 있으면 기존 함수 실행
+    queryKey: ["todos"], // NOTE: 고유 키. 다른 함수 실행할 때만 겹치지 않으면 됨 (겹쳐도 기존에 등록된 함수 실행)
+    queryFn: loadTodos, // NOTE: 함수 매핑
+  });
 
   if (isLoading) {
     return <>Loading . . .</>;
@@ -34,6 +28,8 @@ export default function Home() {
   if (isError) {
     return <>Check error using console</>;
   }
+
+  // NOTE: useState와 useEffect가 사용되지 않는다! (Providers에 있는 state는 서버 상태 전역 관리용이니까 제외하고)
 
   return (
     <div className="container mx-auto px-4 py-8">
