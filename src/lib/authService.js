1bae77b33edb16115f0ba6777214cbcbc1d62118
MyeongjuHong <hmjpearl@gmail.com>
2025-09-25 09:53:35 +0900

    feat: Implement login, logout authService

--- src/lib/authService.js
+++ src/lib/authService.js
@@ -2,8 +2,17 @@ import { defaultFetch, cookieFetch } from "@/lib/fetchClient";
 
 export const authService = {
   // 쿠키 인증을 사용하는 로그인
-  login: (email, password) => {},
+  login: (email, password) =>
+    cookieFetch("auth/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    }),
+
+  // 쿠키를 사용하는 로그아웃
+  logout: () =>
+    cookieFetch("auth/logout", {
+      method: "POST",
+    }),
 
   // 회원가입
-  register: (name, email, password) => {},
 };
--- src/lib/fetchClient.js
+++ src/lib/fetchClient.js
@@ -3,6 +3,10 @@
  */
 export const defaultFetch = async (url, options = {}) => {
   const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+  // NOTE: fetch 기본 옵션
+  // 1. 모든 요청에 JSON 타입 헤더 기본으로 추가
+  // 2. Next.js 기본 캐싱 전략을 정적 캐싱으로 설정
   const defaultOptions = {
     headers: {
       "Content-Type": "application/json",
@@ -11,6 +15,9 @@ export const defaultFetch = async (url, options = {}) => {
     cache: "force-cache",
   };
 
+  // NOTE: 사용자 정의 options와 기본 옵션 병합 (왜..?)
+  // defaultOptions를 먼저 적용 후 사용자가 전달한 options가 있으면 덮어씀
+  // headers 같은 중첩 객체는 spread 후 병합 (아니면 전체가 덮어써짐)
   const mergedOptions = {
     ...defaultOptions,
     ...options,
@@ -42,10 +49,11 @@ export const defaultFetch = async (url, options = {}) => {
 export const cookieFetch = async (url, options = {}) => {
   const baseURL = process.env.NEXT_PUBLIC_API_URL;
   const defaultOptions = {
-    // TODO: 실습내용
-    // 1. 쿠키 전송을 위한 설정
-    // 2. 서버 컴포넌트에서도 매번 재검증을 cache 옵션
-    // 3. Content-Type 헤더 설정
+    headers: {
+      'Content-Type': 'application/json', // Content-Type 헤더 설정
+    },
+    credentials: 'include', // 쿠키 전송을 위한 설정
+    cache: 'no-store', // 서버 컴포넌트에서도 매번 재검증을 캐싱하는 옵션
   };
 
   const mergedOptions = {
